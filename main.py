62c03e61192c327d40f63499b3ad92daa9d09e37
Leon Herrmann <33426049+s0552107@users.noreply.github.com>
2024-09-03 19:24:20 +0200

    init
    Init

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+import requests
+from plankapy import *
+
+def main():
+    # Planka API-Instanz erstellen
+    planka_instance = Planka(
+        url='http://localhost:3000',
+        username='demo',
+        password='demo'
+    )
+    
+    # Project Controller erstellen
+    project_controller = Project(instance=planka_instance)
+    list_controller = List(instance=planka_instance)
+    
+    # Liste der Projektnamen abrufen
+    project_names = project_controller.get_project_names()
+    
+    
+    # Projektnamen ausgeben
+    print("Projekt-Namen:")
+    for name in project_names:
+        print(name)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+from setuptools import setup, find_packages
+
+setup(
+    name='plankapy',
+    version='1.0.0',
+    description='Python library for Planka API',
+    packages=find_packages(),
+)
